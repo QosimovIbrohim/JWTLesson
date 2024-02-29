e860a4c3c82fcae795e806143d7eb70819207b76
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-29 08:22:42 +0500

    Add project files.

---
+++ JWT.Infrastruct/InfrastructDI.cs
@@ -0,0 +1,24 @@
+﻿using JWT.Infrastruct.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT.Infrastruct
+{
+    public static class InfrastructDI
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(ops =>
+            {
+                ops.UseNpgsql(configuration.GetConnectionString("Cs"));
+            });
+            return services;
+        }
+    }
+}
---
+++ JWT.Infrastruct/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using JWT.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT.Infrastruct.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+         : base (options)
+        {
+            Database.Migrate();        
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<GuidSaver> GuidSavers { get; set; }
+    }
+}
